93f885edc981990d4e35a765450a459e5034187c
moavia <mmoavia2002@gmail.com>
2024-07-20 19:19:26 +0500

    authentication added

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
+
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
